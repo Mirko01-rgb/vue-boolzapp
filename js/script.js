952365101574c7305275185ca9b50a09477e562d
Mirko <mirkosbaglia2001@gmail.com>
2021-04-10 14:54:18 +0200

    Inserimento chat con il for

--- js/script.js
+++ js/script.js
@@ -12,11 +12,13 @@ function initVue(){
                     date: '10/01/2020 15:30:55',
                     text: 'Hai portato a spasso il cane?',
                     status: 'sent'
+
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '10/01/2020 ',
                     text: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
+                    status: 'sent',
+                    hour:'15:50'
                 },
                 {
                     date: '10/01/2020 16:15:22',
@@ -36,9 +38,11 @@ function initVue(){
                     status: 'sent'
                 },
                 {
-                    date: '20/03/2020 16:30:55',
+                    date: '20/03/2020 ',
                     text: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
+                    status: 'received',
+                    hour:'16:30'
+
                 },
                 {
                     date: '20/03/2020 16:35:00',
@@ -58,9 +62,11 @@ function initVue(){
                     status: 'received'
                 },
                 {
-                    date: '28/03/2020 10:20:10',
+                    date: '28/03/2020 ',
                     text: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
+                    status: 'sent',
+                    hour:'10:20'
+
                 },
                 {
                     date: '28/03/2020 16:15:22',
@@ -80,9 +86,10 @@ function initVue(){
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '10/01/2020 ',
                     text: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
+                    status: 'received',
+                    hour:'15:50'
                 }
             ],
         },
